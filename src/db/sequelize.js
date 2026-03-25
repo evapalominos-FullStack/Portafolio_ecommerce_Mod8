00ad572837668703f3d3bd6e2de913b52605d28c
Astrid Eva Palominos Espinoza <eva.palominos@gmail.com>
2026-03-24 23:48:41 -0300

    fix: agregar SSL para PostgreSQL en Render

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -6,6 +6,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
